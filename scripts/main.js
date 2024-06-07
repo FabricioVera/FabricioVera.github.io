240e15b39512116ff5f15abc416e45731eeb9685
FabricioVera <fabriciovera7@gmail.com>
2024-06-07 01:59:44 -0300

    adfsasfd

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
-let imagen = document.querySelector("img");
+const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+  };
+
+
+
+let imagen = document.querySelector("#imagenes");
+/*
 imagen.onclick = function () {
     let source = imagen.getAttribute("src");
     if(source==="images/chisato.jpg"){
@@ -7,9 +15,13 @@ imagen.onclick = function () {
         imagen.setAttribute("src","images/chisato.jpg");
     }
 };
+*/
 
-let miBoton = document.querySelector("button");
+
+
+let miBoton = document.querySelector("pasar");
 let miTitulo = document.querySelector("h1");
+
 function estableceNombreUsuario() {
     let miNombre = prompt("Introduzca su nombre.");
     if (miNombre) {
@@ -17,27 +29,77 @@ function estableceNombreUsuario() {
         miTitulo.innerHTML = "Sabías que Chisato es muy linda, " + miNombre + "???";
     }
 }
+
 if (!localStorage.getItem("nombre")) {
     estableceNombreUsuario();
 } else {
     let nombreAlmacenado = localStorage.getItem("nombre");
     miTitulo.textContent = "Sabías que Chisato es muy linda, " + nombreAlmacenado + "?";
 }
+/*
 miBoton.onclick = function () {
     estableceNombreUsuario();
   };
+*/
+
+
+
+//json url
+let url='../jsons/animenames.json'
+//funcion obtiene el json de los datos
+async function getData(url, heads) {
+    url=url.replace(new RegExp(' ', 'g'), '%20');
+    console.log(url);
+    const response = await fetch(url, heads);
+    console.log(response);
+    return response.json();
+}
+
+let data = await getData(url,requestOptions);
+console.log({ data });
+
+// lista de nombres de animes
+let animes=data["animenames"];
+
+console.log(animes);
+
+const datalist = document.getElementById('anime-names')
+animes.forEach((obj) => {
+    const option = document.createElement('option');
+    option.value = obj;
+    datalist.appendChild(option);
+})
+
+ //1. espera a que se presione el boton
+
+const btnpasar=document.querySelector("#pasar");
+const maxanime = 1000;
+let numeroaleatorio=Math.floor(Math.random() * maxanime);
+
+let randomanimename=String(animes[numeroaleatorio]);
+
+
   
-const myHeaders = new Headers();
-myHeaders.append("X-MAL-CLIENT-ID", "cbbf880a5df1aa4ce57752f503668026");
-myHeaders.append("Access-Control-Allow-Origin", "*");
+url="https://api.jikan.moe/v4/anime?q="+randomanimename
 
-const requestOptions = {
-method: "GET",
-headers: myHeaders,
-redirect: "follow"
-};
+data = await getData(url,requestOptions);
+console.log({ data });
+let randomanimeid = data["data"][0]["mal_id"];
+console.log(randomanimeid);
 
-fetch("https://api.myanimelist.net/v2/users/Fabricckk/animelist?status=completed&limit=10", requestOptions)
-.then((response) => response.text())
-.then((result) => console.log(result))
-.catch((error) => console.error(error));
+
+// 2. guardar el nombre y obtener su anime y la imagen
+
+url="https://api.jikan.moe/v4/anime/"+randomanimeid+"/characters";
+data = await getData(url,requestOptions);
+console.log({data});
+let maxcharacter=Object.keys(data["data"]).length;
+numeroaleatorio=Math.floor(Math.random() * maxcharacter);
+let randomcharactername=data["data"][numeroaleatorio]["character"]["name"];
+
+let randomcharacterimage=data["data"][numeroaleatorio]["character"]["name"]["images"];
+console.log(data["data"][numeroaleatorio]["character"]["images"]["webp"]["image_url"]);
+
+// 3. reemplazar la imagen en el medio
+imagen.setAttribute("src",data["data"][numeroaleatorio]["character"]["images"]["webp"]["image_url"]);
+// 4. esperar respuesta
